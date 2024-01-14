fe17d1152ce909a8ac889255b425ed35665195e8
Yury Komusov <yurykomusov@coherentsolutions.com>
2024-01-14 11:33:29 +0200

    just collect numbers, not strings

--- 2023/playground.fsx
+++ 2023/playground.fsx
@@ -1,3 +1,5 @@
+open System
+open System.IO
 
 let parseLine(input : string) : string * (int array) = 
     let (str, freq) = 
@@ -22,50 +24,53 @@ let combine (leftPadSize : int) (head : int) (rightPadSize : int) =
     |> Array.concat 
     |> String.concat ""
 
-let rec generate (pattern : string) (freq : int list) (size : int) (startIndex : int) : string list =   
+let rec generate (pattern : string) (freq : int list) (size : int) (startIndex : int) : int =   
     let maxPadSize = size - (freq |> List.sum) - (freq.Length - 1)
     let minPadSize = if startIndex = 0 then 0 else 1
 
     let generate' = generate pattern
 
-    
-
     match freq with
-    | [] -> List.empty
+    | [] -> 0
     | [head] -> 
         [minPadSize..maxPadSize]
         |> List.map (fun padSize -> combine padSize head (size - (padSize + head)))
         |> List.filter (fun prefix -> testPattern prefix (pattern.Substring(startIndex, prefix.Length)))
+        |> List.length
     | head :: tail -> 
         [minPadSize..maxPadSize] 
         |> List.rev
         |> List.map (fun p -> (p, combine p head 0))
         |> List.filter (fun (_, prefix) -> testPattern prefix (pattern.Substring(startIndex, prefix.Length))) 
         |> List.map (fun (padSize, prefix) -> 
-            let generated = generate' tail (size - (padSize + head)) (startIndex + padSize + head)
-            
-            generated |> List.map (fun g -> prefix + g))
-        |> List.collect (fun s -> s)
+            generate' tail (size - (padSize + head)) (startIndex + padSize + head)
+        )
+            //generated |> List.map (fun g -> prefix + g))
+        //|> List.collect (fun s -> s)
+        |> List.sum
     
 
 let sw = System.Diagnostics.Stopwatch()
 
 sw.Start()
 
-let lines = @"???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1"
+let path = Path.Combine(__SOURCE_DIRECTORY__, "Day12.txt")
+let lines = File.ReadAllLines(path)
 
-lines.Split(System.Environment.NewLine)
+// let lines = @"???.### 1,1,3
+// .??..??...?##. 1,1,3
+// ?#?#?#?#?#?#?#? 1,3,1,6
+// ????.#...#... 4,1,1
+// ????.######..#####. 1,6,5
+// ?###???????? 3,2,1"
+
+lines
     |> Array.map parseLine
     |> Array.iter(fun (pattern, freq) -> 
-        let pattern' = pattern |> Array.replicate 5 |> String.concat "?"
-        let freq' = freq |> Array.replicate 5 |> Array.concat |> Array.toList
+        let pattern' = pattern |> Array.replicate 4 |> String.concat "?"
+        let freq' = freq |> Array.replicate 4 |> Array.concat |> Array.toList
 
-        generate pattern' freq' pattern'.Length 0 |> List.length |> printfn "result %d"
+        generate pattern' freq' pattern'.Length 0 |> printfn "result %d"
     )
 
 sw.Stop()
